24d82281aefce3a8431c63ca5fc0386fa1645776
Richard Wells <Richard.Wells@normandale.edu>
2019-01-30 20:49:28 -0600

    Added subrange versions of sort algorithms.

--- Cpp/StdSort.h
+++ Cpp/StdSort.h
@@ -10,23 +10,35 @@ namespace CSn
 {
 
 template<typename T, typename C = std::less<>>
+void selection_sort(Array<T> & a, index lo, index hi, C cmp = C())
+{
+	for (index i = lo; i < hi; i++)
+	{
+		index min_idx = min_element(a, i, hi, cmp);
+		swap(a, i, min_idx);
+	}
+}
+
+template<typename T, typename C = std::less<>>
 void selection_sort(Array<T> & a, C cmp = C())
 {
-	for (index i = 0; i < a.size(); i++)
+	selection_sort(a, 0, a.size(), cmp);
+}
+
+template<typename T, typename C = std::less<>>
+void insertion_sort(Array<T> & a, index lo, index hi, C cmp = C())
+{
+	for (index i = lo + 1; i < hi; i++)
 	{
-		index min_idx = min_element(a, i, a.size(), cmp);
-		swap(a, i, min_idx);
+		index idx = upper_bound(a, lo, i, a[i], cmp);
+		rotate_right(a, idx, i + 1);
 	}
 }
 
 template<typename T, typename C = std::less<>>
 void insertion_sort(Array<T> & a, C cmp = C())
 {
-	for (index i = 1; i < a.size(); i++)
-	{
-		index idx = upper_bound(a, 0, i, a[i], cmp);
-		rotate_right(a, idx, i+1);
-	}
+	insertion_sort(a, 0, a.size(), cmp);
 }
 
 }
