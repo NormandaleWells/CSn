1ef9ef02610a02ab9635544be8ad4b41cfa79aa4
Richard Wells <Richard.Wells@normandale.edu>
2021-01-30 15:31:02 -0600

    Renamed DisplayComponents for consistency.

--- Java/CSn/edu/normandale/csn/DisplayComponents.java
+++ Java/CSn/edu/normandale/csn/DisplayComponents.java
@@ -1,6 +1,6 @@
 package edu.normandale.csn;
 
-public class DisplayIntComponents {
+public class DisplayComponents {
 
 	// '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231'
 	// '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe'
