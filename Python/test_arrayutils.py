1ff6a9daf8c197186c9098267be9c2325a28dd51
Richard Wells <Richard.Wells@normandale.edu>
2021-02-21 16:00:25 -0600

    Replaced lambda with def, per PEP8 E371.

--- Python/test_arrayutils.py
+++ Python/test_arrayutils.py
@@ -1,10 +1,10 @@
 import unittest
 import arrayutils
 
+
 class TestFindFunctions(unittest.TestCase):
 
     def test_find(self):
-
         # test full array
         a = [27, 82, 41, 124, 62, 31, 94, 47]
         self.assertEqual(arrayutils.find(a, 27), 0)
@@ -15,10 +15,10 @@ class TestFindFunctions(unittest.TestCase):
         # test subrange
         self.assertEqual(arrayutils.find(a, 27, lo=2, hi=6), -1)
         self.assertEqual(arrayutils.find(a, 82, lo=2, hi=6), -1)
-        self.assertEqual(arrayutils.find(a, 41, lo=2, hi=6),  2)
-        self.assertEqual(arrayutils.find(a, 31, lo=2, hi=6),  5)
+        self.assertEqual(arrayutils.find(a, 41, lo=2, hi=6), 2)
+        self.assertEqual(arrayutils.find(a, 31, lo=2, hi=6), 5)
         self.assertEqual(arrayutils.find(a, 94, lo=2, hi=6), -1)
-        self.assertEqual(arrayutils.find(a,  0, lo=2, hi=6), -1)
+        self.assertEqual(arrayutils.find(a, 0, lo=2, hi=6), -1)
 
         # test full array with multiple equal values
         a = [2, 2, 3, 4, 3, 5]
@@ -33,14 +33,16 @@ class TestFindFunctions(unittest.TestCase):
 
     def test_find_if(self):
         a = [27, 82, 41, 124, 62, 31, 94, 47]
-        pred = lambda n : n > 100
-        self.assertEqual(arrayutils.find_if(a, pred), 3)
-        self.assertEqual(arrayutils.find_if(a, pred, lo=2, hi=6), 3)
-        self.assertEqual(arrayutils.find_if(a, pred, lo=0, hi=3), -1)
-        self.assertEqual(arrayutils.find_if(a, pred, lo=4, hi=-1), -1)
+
+        def greater_than_100(n):
+            return n > 100
+
+        self.assertEqual(arrayutils.find_if(a, greater_than_100), 3)
+        self.assertEqual(arrayutils.find_if(a, greater_than_100, lo=2, hi=6), 3)
+        self.assertEqual(arrayutils.find_if(a, greater_than_100, lo=0, hi=3), -1)
+        self.assertEqual(arrayutils.find_if(a, greater_than_100, lo=4, hi=-1), -1)
 
     def test_count(self):
-
         # test full array
         a = [1, 2, 3, 2, 1, 2, 3]
         self.assertEqual(arrayutils.count(a, 1), 2)
@@ -53,10 +55,14 @@ class TestFindFunctions(unittest.TestCase):
 
     def test_count_if(self):
         a = [1, 2, 3, 2, 1, 2, 3]
-        is_even = lambda n : n % 2 == 0
+
+        def is_even(n):
+            return n % 2 == 0;
+
         self.assertEqual(arrayutils.count_if(a, is_even), 3)
         self.assertEqual(arrayutils.count_if(a, is_even, lo=1, hi=6), 3)
         self.assertEqual(arrayutils.count_if(a, is_even, lo=2, hi=5), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
