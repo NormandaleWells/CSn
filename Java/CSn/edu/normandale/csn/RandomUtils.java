5ca4cc8ed0e7957037768f674cdc1b49294dd769
Richard Wells <Richard.Wells@normandale.edu>
2021-05-31 21:24:46 -0500

    Made functions public.  D'oh!

--- Java/CSn/edu/normandale/csn/RandomUtils.java
+++ Java/CSn/edu/normandale/csn/RandomUtils.java
@@ -13,25 +13,25 @@ public final class RandomUtils {
 
 	static Random rand = new Random();
 
-	static void setSeed(long seed) {
+	public static void setSeed(long seed) {
 		rand.setSeed(seed);
 	}
 
 	// Returns a value in the half-open [lo,hi).
-	static int uniform(int lo, int hi) {
+	public static int uniform(int lo, int hi) {
 		return lo + (int)(rand.nextDouble() * (hi - lo));
 	}
 	
-	static int uniform(int n) {
+	public static int uniform(int n) {
 		return uniform(0, n);
 	}
 
-	static boolean bernoulli(double p) {
+	public static boolean bernoulli(double p) {
 		return rand.nextDouble() < p;
 	}
 	
 	// Adapted from Sedgewick and Wayne's _Algorithms 4th Edition_, page 32.
-	static <T> void shuffle(T[] a) {
+	public static <T> void shuffle(T[] a) {
 		int n = a.length;
 		for (int i = 0; i < n; i++) {
 			int r = i + uniform(n-i);
