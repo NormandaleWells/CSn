5f26e5d0973bbc8cb11f9f123c0412972901f943
NormandaleWells <Richard.Wells@normandale.edu>
2025-03-02 10:57:13 -0600

    Fixed some typos in OrderedMap.
    These were left over from when it was changed form ST to Map.

--- Java/CSn/edu/normandale/csn/OrderedMap.java
+++ Java/CSn/edu/normandale/csn/OrderedMap.java
@@ -5,7 +5,7 @@ package edu.normandale.csn;
  * map.  This API is based on the ST API defined
  * by Sedgewick and Wayne in _Algorithms, 4th edition_.
  * <br><br>
- * A symbol is a collection of key/value pairs that allows
+ * A map is a collection of key/value pairs that allows
  * lookup of a value based on a given key.  A given key
  * may have only a single value (that is, this is not a
  * multimap), but that value may be a collection type.
@@ -24,21 +24,21 @@ package edu.normandale.csn;
  * 
  * @author rdwells
  *
- * @param <Key>	The type used for the keys in the table.
- * @param <Value> The type used for the values in the table.
+ * @param <Key>	The type used for the keys in the map.
+ * @param <Value> The type used for the values in the map.
  */
 interface OrderedMap<Key extends Comparable<Key>, Value> extends Map<Key, Value> {
 	/**
-	 * Returns the minimum key in the table, or <tt>null</tt> if the table is empty.
+	 * Returns the minimum key in the map, or <tt>null</tt> if the map is empty.
 	 * 
-	 * @return		The minimum value of any key in the table.
+	 * @return		The minimum value of any key in the map.
 	 */
 	Key min();
 	
 	/**
-	 * Returns the maximum key in the table, or <tt>null</tt> if the table is empty.
+	 * Returns the maximum key in the map, or <tt>null</tt> if the map is empty.
 	 * 
-	 * @return		The maximum value of any key in the table.
+	 * @return		The maximum value of any key in the map.
 	 */
 	Key max();
 	
@@ -53,7 +53,7 @@ interface OrderedMap<Key extends Comparable<Key>, Value> extends Map<Key, Value>
 	
 	/**
 	 * Returns the smallest key strictly greater than the
-	 * given key. If all the keys in the table are less than
+	 * given key. If all the keys in the map are less than
 	 * the given key, <tt>null</tt> is returned.
 	 * 
 	 * @param k		The key to search for.
