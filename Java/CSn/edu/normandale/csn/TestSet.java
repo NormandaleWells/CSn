5059aaaf509e5678efbfec07aceb2683bf427914
NormandaleWells <Richard.Wells@normandale.edu>
2025-03-02 10:36:47 -0600

    Fleshed out TestSet.
    This is actually the old TestBag renamed to to TestSet, with tests added for removal of items.

--- Java/CSn/edu/normandale/csn/TestSet.java
+++ Java/CSn/edu/normandale/csn/TestSet.java
@@ -6,34 +6,77 @@ import org.junit.Test;
 
 public class TestSet {
 
-	private void testBag(Set<String> b) {
-		assertTrue(b.isEmpty());
-		assertEquals(b.size(), 0);
+	private void testSet(Set<String> set) {
+		assertTrue(set.isEmpty());
+		assertEquals(set.size(), 0);
 
-		b.add("A");
-		assertFalse(b.isEmpty());
-		assertEquals(b.size(), 1);
+		set.add("A");
+		assertFalse(set.isEmpty());
+		assertEquals(set.size(), 1);
+	
+		set.add("B");
+		set.add("C");
+		assertFalse(set.isEmpty());
+		assertEquals(set.size(), 3);
 		
-		b.add("B");
-		b.add("C");
-		assertFalse(b.isEmpty());
-		assertEquals(b.size(), 3);
-		
+		set.add("C");
+		set.add("A");
+		set.add("B");
+		assertFalse(set.isEmpty());
+		assertEquals(set.size(), 3);
+
 		String[] expected = { "A", "B", "C" };
-		String[] items = TestUtils.iterToArray(b, b.size());
+		String[] items = TestUtils.iterToArray(set, set.size());
 		Arrays.sort(items);
-		org.junit.Assert.assertArrayEquals(expected, items);
+		assertArrayEquals(expected, items);
+
+		set.add("D");
+		set.add("E");
+		set.add("F");
+		assertFalse(set.isEmpty());
+		assertEquals(set.size(), 6);
+
+		set.remove("D");
+		set.remove("A");
+		set.remove("F");
+		assertFalse(set.isEmpty());
+		assertEquals(set.size(), 3);
+
+		set.remove("F");
+		set.remove("A");
+		assertFalse(set.isEmpty());
+		assertEquals(set.size(), 3);
+
+		String[] expected2 = { "B", "C", "E" };
+		items = TestUtils.iterToArray(set, set.size());
+		Arrays.sort(items);
+		assertArrayEquals(expected2, items);
+
+		set.remove("E");
+		set.remove("B");
+		set.remove("C");
+		assertTrue(set.isEmpty());
+		assertEquals(set.size(), 0);
+
+		set.remove("G");
+		assertTrue(set.isEmpty());
+		assertEquals(set.size(), 0);
+
+		set.add("G");
+		set.add("H");
+		assertFalse(set.isEmpty());
+		assertEquals(set.size(), 2);
 	}
 
 	@Test
 	public void testUnorderedArraySet() {
 		Set<String> b = new UnorderedArraySet<>();
-		testBag(b);
+		testSet(b);
 	}
 
 	@Test
 	public void testLinkedSet() {
 		Set<String> b = new LinkedSet<>();
-		testBag(b);
+		testSet(b);
 	}
 }
