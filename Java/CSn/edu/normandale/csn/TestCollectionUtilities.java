d2d62c5bdbeee54419a08fd59e9017398708de8b
NormandaleWells <Richard.Wells@normandale.edu>
2025-03-02 09:55:58 -0600

    Rewrote interactive tests to allow more options.

--- Java/CSn/edu/normandale/csn/TestCollectionUtilities.java
+++ Java/CSn/edu/normandale/csn/TestCollectionUtilities.java
@@ -9,47 +9,172 @@ public final class TestCollectionUtilities {
 	}
 
 	private static void printCollection(String colType, Iterable<String> collection) {
-		System.out.printf("\nFinal %s:\n", colType);
 		for (String s : collection)
-			System.out.printf("\t%s\n", s);
+			System.out.printf("%s ", s);
+		System.out.println();
+	}
+
+	private static void printTestQueueUsage() {
+		System.out.println("Commands:");
+		System.out.println("\tnqueue <data> (add, a, +)");
+		System.out.println("\tdequeue       (remove, r, -)");
+		System.out.println("\tsize          (s)");
+		System.out.println("\tisEmpty       (e)");
+		System.out.println("\tprint         (p)");
 	}
 
 	public static void testQueue(Queue<String> queue) {
+		printTestQueueUsage();
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()) {
-			String s = scanner.next();
-			if (s.equals("-"))
-				System.out.printf("%s ", queue.dequeue());
-			else
-				queue.enqueue(s);
+		while (scanner.hasNextLine()) {
+			String s = scanner.nextLine();
+			String[] fields = s.split(" ");
+			if (fields.length == 0)
+				continue;
+
+			String command = fields[0];
+			String data = fields.length > 1 ? fields[1] : null;
+			switch (command) {
+				case "enqueue":
+				case "add":
+				case "a":
+				case "+":
+					queue.enqueue(data);
+					break;
+				case "dequeue":
+				case "remove":
+				case "r":
+				case "-":
+					data = queue.dequeue();
+					System.out.println(data);
+					break;
+				case "size":
+				case "s":
+					System.out.println(queue.size());
+					break;
+				case "isEmpty":
+				case "e":
+					System.out.println(queue.isEmpty());
+					break;
+				case "print":
+				case "p":
+					printCollection("set", queue);
+					break;
+				default:
+					printTestSetUsage();
+			}
 		}
 		scanner.close();
 
 		printCollection("queue", queue);
 	}
 
-	public static void testStack(Stack<String> stack) {
-		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()) {
-			String s = scanner.next();
-			if (s.equals("-"))
-				System.out.printf("%s ", stack.pop());
-			else
-				stack.push(s);
-		}
-		scanner.close();
-		
-		printCollection("stack", stack);
+	private static void printTestStackUsage() {
+		System.out.println("Commands:");
+		System.out.println("\tpush <data> (add, a, +)");
+		System.out.println("\tpop         (remove, r, -)");
+		System.out.println("\tpeek        (k)");
+		System.out.println("\tsize        (s)");
+		System.out.println("\tisEmpty     (e)");
+		System.out.println("\tprint       (p)");
 	}
 
-	public static void testSet(Set<String> bag) {
+	public static void testStack(Stack<String> stack) {
+		printTestStackUsage();
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()) {
-			String s = scanner.next();
-			bag.add(s);
+		while (scanner.hasNextLine()) {
+			String s = scanner.nextLine();
+			String[] fields = s.split(" ");
+			if (fields.length == 0)
+				continue;
+
+			String command = fields[0];
+			String data = fields.length > 1 ? fields[1] : null;
+			switch (command) {
+				case "push":
+				case "add":
+				case "a":
+				case "+":
+					stack.push(data);
+					break;
+				case "pop":
+				case "remove":
+				case "r":
+				case "-":
+					data = stack.pop();
+					System.out.println(data);
+					break;
+				case "peek":
+				case "k":
+					data = stack.peek();
+					System.out.println(data);
+					break;
+				case "size":
+				case "s":
+					System.out.println(stack.size());
+					break;
+				case "isEmpty":
+				case "e":
+					System.out.println(stack.isEmpty());
+					break;
+				case "print":
+				case "p":
+					printCollection("set", stack);
+					break;
+				default:
+					printTestSetUsage();
+			}
 		}
 		scanner.close();
+	}
 
-		printCollection("bag", bag);
+	private static void printTestSetUsage() {
+		System.out.println("Commands:");
+		System.out.println("\tadd <data>    (a, +)");
+		System.out.println("\tremove <data> (r, -)");
+		System.out.println("\tsize          (s)");
+		System.out.println("\tisEmpty       (e)");
+		System.out.println("\tprint         (p)");
+	}
+
+	public static void testSet(Set<String> set) {
+		printTestSetUsage();
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNextLine()) {
+			String s = scanner.nextLine();
+			String[] fields = s.split(" ");
+			if (fields.length == 0)
+				continue;
+
+			String command = fields[0];
+			String data = fields.length > 1 ? fields[1] : null;
+			switch (command) {
+				case "add":
+				case "a":
+				case "+":
+					set.add(data);
+					break;
+				case "remove":
+				case "r":
+				case "-":
+					set.remove(data);
+					break;
+				case "size":
+				case "s":
+					System.out.println(set.size());
+					break;
+				case "isEmpty":
+				case "e":
+					System.out.println(set.isEmpty());
+					break;
+				case "print":
+				case "p":
+					printCollection("set", set);
+					break;
+				default:
+					printTestSetUsage();
+			}
+		}
+		scanner.close();
 	}
 }
