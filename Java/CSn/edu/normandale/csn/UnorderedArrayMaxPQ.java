78ed961b09a9048d1a06a777fabed30bae8c9597
NormandaleWells <Richard.Wells@normandale.edu>
2025-03-28 13:15:46 -0500

    Fixed formatting.
    Tabs were somehow set to two spaces.  Autoformatting fixed a few other things as well.

--- Java/CSn/edu/normandale/csn/UnorderedArrayMaxPQ.java
+++ Java/CSn/edu/normandale/csn/UnorderedArrayMaxPQ.java
@@ -2,61 +2,61 @@ package edu.normandale.csn;
 
 import java.util.Iterator;
 
-public class UnorderedArrayMaxPQ <T extends Comparable<T>> implements MaxPQ<T>, Iterable<T> {
+public class UnorderedArrayMaxPQ<T extends Comparable<T>> implements MaxPQ<T>, Iterable<T> {
 
-	@SuppressWarnings("unchecked")
-	private T[] q = (T[]) new Comparable[4];
-	private int count = 0;
+    @SuppressWarnings("unchecked")
+    private T[] q = (T[]) new Comparable[4];
+    private int count = 0;
 
-	@SuppressWarnings("unchecked")
-	private void resize(int newSize) {
-		assert(newSize >= count);
-		T[] newQ = (T[]) new Comparable[newSize];
-		ArrayUtils.move(q, newQ, 0, count);
-		q = newQ;
-	}
+    @SuppressWarnings("unchecked")
+    private void resize(int newSize) {
+        assert (newSize >= count);
+        T[] newQ = (T[]) new Comparable[newSize];
+        ArrayUtils.move(q, newQ, 0, count);
+        q = newQ;
+    }
 
     @Override
     public void insert(T value) {
-		if (count == q.length)
-			resize(q.length * 2);
-		q[count++] = value;
+        if (count == q.length)
+            resize(q.length * 2);
+        q[count++] = value;
     }
 
     @Override
     public T max() {
-		if (count == 0)
-			throw new RuntimeException("UnorderedArrayMaxPQ underflow");
+        if (count == 0)
+            throw new RuntimeException("UnorderedArrayMaxPQ underflow");
         int idx = ArrayUtils.maxElement(q, 0, count);
-		return q[idx];
+        return q[idx];
     }
 
     @Override
     public T delMax() {
-		if (count == 0)
-			throw new RuntimeException("OrderedArrayMaxPQ underflow");
-
+        if (count == 0) {
+            throw new RuntimeException("OrderedArrayMaxPQ underflow");
+        }
         // Since the array is unordered, there's no need to remove the max
-        // from the middle and shift the elements.  Instead, we just exchange
+        // from the middle and shift the elements. Instead, we just exchange
         // the max with the last item, and remove it from the end.
         int idx = ArrayUtils.maxElement(q, 0, count);
-        ArrayUtils.swap(q, idx, count-1);
-		T max = q[--count];
+        ArrayUtils.swap(q, idx, count - 1);
+        T max = q[--count];
 
-		if (q.length > 4 && count <= q.length / 4) {
-			resize(q.length / 2);
-		}
-		return max;
+        if (q.length > 4 && count <= q.length / 4) {
+            resize(q.length / 2);
+        }
+        return max;
     }
 
     @Override
     public boolean isEmpty() {
-		return count == 0;
+        return count == 0;
     }
 
     @Override
     public int size() {
-		return count;
+        return count;
     }
 
     @Override
@@ -65,6 +65,6 @@ public class UnorderedArrayMaxPQ <T extends Comparable<T>> implements MaxPQ<T>, 
         T[] qCopy = (T[]) new Comparable[count];
         Sort.quicksort(qCopy);
         ArrayUtils.copy(q, qCopy, 0, count);
-            return ArrayUtils.getBackwardIterator(q, 0, count);
-    }    
+        return ArrayUtils.getBackwardIterator(q, 0, count);
+    }
 }
